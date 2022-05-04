dc0af143629fdfa0cbf59fb52c0e19a0f682791b
Timoté LANCELLE <timote.lancelle@outlook.fr>
2022-05-04 19:21:04 +0200

    Correction séléction des locations, ajout gestion dynamique de la couleur du texte et des champs quand informations corrects ou incorrects

--- js/modal.js
+++ js/modal.js
@@ -18,17 +18,17 @@ const lastName = document.querySelector("#lastName");
 const email = document.querySelector("#email");
 const birthdate = document.querySelector("#birthdate");
 const quantity = document.querySelector("#quantity");
-const locationTournament = document.getElementsByName("location");
+const locationTournament = document.querySelector(".location");
 const terms = document.querySelector("#checkbox1");
 const btnSubmit = document.querySelector("#submit");
 
-const firstNameError = document.querySelector("#firstNameError");
-const lastNameError = document.querySelector("#lastNameError");
-const emailError = document.querySelector("#emailError");
-const birthdateError = document.querySelector("#birthdateError");
-const quantityError = document.querySelector("#quantityError");
-const locationError = document.querySelector("#locationError");
-const termsError = document.querySelector("#termsError");
+const firstNameValidation = document.querySelector("#firstNameValidation");
+const lastNameValidation = document.querySelector("#lastNameValidation");
+const emailValidation = document.querySelector("#emailValidation");
+const birthdateValidation = document.querySelector("#birthdateValidation");
+const quantityValidation = document.querySelector("#quantityValidation");
+const locationValidation = document.querySelector("#locationValidation");
+const termsValidation = document.querySelector("#termsValidation");
 
 //Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -48,13 +48,46 @@ function regExLastnameFirstname(value) {
     return /^([A-Za-z]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(value);    //la méthode test() vérifie s'il y a une correspondance entre un texte et une expression rationnelle.
 }                                                                           //elle retourne true en succès, et false en cas contraire (booléen).
 
+//Texte validation ok
+function textCorrectForm(querySelectorId){
+    document.querySelector(`#${querySelectorId}`).innerHTML = "Valide";
+}
+
+//Couleur texte champ correct
+function colorTextCorrectForm(querySelectorId) {
+    document.querySelector(`#${querySelectorId}`).classList.add("div-valide");
+    document.querySelector(`#${querySelectorId}`).classList.remove("div-error");
+}
+
+//Couleur texte champ incorrect
+function colorTextIncorrectForm(querySelectorId) {
+    document.querySelector(`#${querySelectorId}`).classList.add("div-error");
+    document.querySelector(`#${querySelectorId}`).classList.remove("div-valide");
+}
+
+//Couleur background champ correct
+function colorBackgroundCorrectForm(querySelectorId) {
+    document.querySelector(`#${querySelectorId}`).classList.add("background-valide");
+    document.querySelector(`#${querySelectorId}`).classList.remove("background-error");
+}
+
+//Couleur background champ incorrect
+function colorBackgroundIncorrectForm(querySelectorId) {
+    document.querySelector(`#${querySelectorId}`).classList.add("background-error");
+    document.querySelector(`#${querySelectorId}`).classList.remove("background-valide");
+}
+
 //Contrôle de la validité du prénom
 function firstNameControl() {
     if (regExLastnameFirstname(firstName.value)) {
-        firstNameError.innerHTML = "Valide";
+        textCorrectForm("firstNameValidation");
+        colorTextCorrectForm("firstNameValidation");
+        colorBackgroundCorrectForm("firstName");
         return true;
     } else {
-        firstNameError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+        firstNameValidation.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+        colorTextIncorrectForm("firstNameValidation");
+        colorBackgroundIncorrectForm("firstName");
         return false;
     }
 }
@@ -62,10 +95,14 @@ function firstNameControl() {
 //Contrôle de la validité du nom
 function lastNameControl() {
     if (regExLastnameFirstname(lastName.value)) {
-        lastNameError.innerHTML = "Valide";
+        textCorrectForm("lastNameValidation");
+        colorTextCorrectForm("lastNameValidation");
+        colorBackgroundCorrectForm("lastName");
         return true;
     } else {
-        lastNameError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+        lastNameValidation.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+        colorTextIncorrectForm("lastNameValidation");
+        colorBackgroundIncorrectForm("lastName");
         return false;
     }
 }
@@ -73,10 +110,14 @@ function lastNameControl() {
 //Contrôle de la validité de l'email
 function emailControl() {
     if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)) {
-        emailError.innerHTML = "Valide";
+        textCorrectForm("emailValidation");
+        colorTextCorrectForm("emailValidation");
+        colorBackgroundCorrectForm("email");
         return true;
     } else {
-        emailError.innerHTML = "Veuillez entrer un email valide.";
+        emailValidation.innerHTML = "Veuillez entrer un email valide.";
+        colorTextIncorrectForm("emailValidation");
+        colorBackgroundIncorrectForm("email");
         return false;
     }
 }
@@ -84,10 +125,14 @@ function emailControl() {
 //Contrôle de la validité de la date de naissance
 function birthdateControl() {
     if(birthdate.value === '' || birthdate.value == null) {
-        birthdateError.innerHTML = "Veuillez entrer votre date de naissance.";
+        birthdateValidation.innerHTML = "Veuillez entrer votre date de naissance.";
+        colorTextIncorrectForm("birthdateValidation");
+        colorBackgroundIncorrectForm("birthdate");
         return false;
     } else {
-        birthdateError.innerHTML = "Valide";
+        textCorrectForm("birthdateValidation");
+        colorTextCorrectForm("birthdateValidation");
+        colorBackgroundCorrectForm("birthdate");
         return true;
     }
 }
@@ -95,10 +140,14 @@ function birthdateControl() {
 //Contrôle nombre tournoi
 function quantityControl() {
     if(quantity.value == '') {
-        quantityError.innerHTML = "Veuillez indiquer le nombre de tournoi.";
+        quantityValidation.innerHTML = "Veuillez indiquer le nombre de tournoi.";
+        colorTextIncorrectForm("quantityValidation");
+        colorBackgroundIncorrectForm("quantity");
         return false;
     } else {
-        quantityError.innerHTML = "Valide";
+        textCorrectForm("quantityValidation");
+        colorTextCorrectForm("quantityValidation");
+        colorBackgroundCorrectForm("quantity");
         return true;
     }
 }
@@ -106,10 +155,12 @@ function quantityControl() {
 //Contrôle choix ville
 function locationTournamentControl() {
     if(locationTournament.checked == false) {
-        locationError.innerHTML = "Veuillez cocher une ville.";
+        locationValidation.innerHTML = "Veuillez cocher une ville.";
+        colorTextIncorrectForm("locationValidation");
         return false;
     } else {
-        locationError.innerHTML = "Valide";
+        textCorrectForm("locationValidation");
+        colorTextCorrectForm("locationValidation");
         return true;
     }
 }
@@ -117,10 +168,12 @@ function locationTournamentControl() {
 //Contrôle conditions acceptés
 function termsControl() {
     if(terms.checked == false) {
-        termsError.innerHTML = "Veuillez accepter les conditions d'utilisation.";
+        termsValidation.innerHTML = "Veuillez accepter les conditions d'utilisation.";
+        colorTextIncorrectForm("termsValidation");
         return false;
     } else {
-        termsError.innerHTML = "Valide";
+        textCorrectForm("termsValidation");
+        colorTextCorrectForm("termsValidation");
         return true;
     }
 }
@@ -128,7 +181,6 @@ function termsControl() {
 //Événement au click bouton submit
 btnSubmit.addEventListener("click", function(event) {
     event.preventDefault();
-
     //Contrôle validité du formulaire
     if(firstNameControl() && lastNameControl() && emailControl() && birthdateControl() && quantityControl() && locationTournamentControl() && termsControl()) {
         alert("Tout est valide");
